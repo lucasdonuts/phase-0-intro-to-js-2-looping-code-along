c318f02bc26889b8148e20479478d364758b6522
Lucas Duncan <lucasdonuts@gmail.com>
2022-06-28 16:33:32 -0500

    commit tree d674b2af7c99e55dbe395ef1ec7c7a10bb3671b5

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thankYous = [];
+
+    for (let i = 0; i < names.length; i++) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+
+    return thankYous;
+}
+
+function countDown(num) {
+    while(num + 1) {
+        console.log(num);
+        num -= 1;
+    }
+}
+
+countDown(10);
